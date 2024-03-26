a38831fe95eb10d9f1922e4d79674c1626c2587e
Neyroph <116799727+Neyroph@users.noreply.github.com>
2024-03-26 15:52:39 +0100

    update

--- Reseau/shared/npings/r3/m_dsi_npings.bat
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo -e "\033[0;33m> Test de la connexion entre les machines DSI et le serveur S en SSH (tcp port 22)\033[0;0m"
-nping -c 1 --tcp --dest-port 22 --source-port 22 --dest-ip 172.12.150.1
--- without_iptables/shared/npings/m_employé_npings.bat
+++
@@ -1,4 +0,0 @@
-#test connection employé sur serveur S port 1234
-#test connexion employé sur serveur S en SFTP port 22
-
-#test connexion employé internet 80,443,25
